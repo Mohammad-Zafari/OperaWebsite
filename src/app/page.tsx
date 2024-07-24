c4ceff83a2fa543e9a7130364c26915f418a8cff
Mohammad Zafari <my.mohammad.zafari@gmail.com>
2024-07-24 10:25:57 +0330

    importing components to the main page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
-
 "use client"; // Ensures this component is client-side
 
 import React from "react";
-
+import Carts from "../components/carts/carts";
+import ContactUs from "../components/contactUs";
+import Footer from "../components/Footer";
 
 const Home = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Carts />
+      <ContactUs />
+      <Footer />
+    </div>
+  );
 };
 
 export default Home;
