038ddb5233e55acff69226a23493bf59a2642e87
AmirMahdi80 <m.ikani1380@gmail.com>
2024-07-31 16:53:39 +0330

    add example code to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,25 @@
+// src/app/page.tsx
 
-"use client"; // Ensures this component is client-side
+"use client";
 
 import React from "react";
-
+import BlogList from "../components/Blog/BlogList";
+import { blogPosts } from "../helper/data";
+import ImageCarousel from "@/components/CustomersInfo/ImageCarousel";
+import GoogleLoginHandler from "@/components/GoogleLoginHandler";
 
 const Home = () => {
-  
-
   return (
-    <div>home</div>
+    <div>
+      {/* <BlogList blogPosts={blogPosts} />
+      <div className="flex flex-col items-center justify-center min-h-screen py-2">
+        <h1 className="text-4xl text-center mt-10 mb-10">
+          Welcome to My Next.js App
+        </h1>
+        <ImageCarousel />
+        <GoogleLoginHandler />
+      </div> */}
+    </div>
   );
 };
 
