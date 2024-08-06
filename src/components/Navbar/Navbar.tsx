20272b9467b2c9cb9aa7b09216618aaa668f4ad7
Mohammad Zafari <my.mohammad.zafari@gmail.com>
2024-08-06 17:23:46 +0330

    adding dropdown navbar component which shows when we log in

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,27 +1,35 @@
-
-"use client"
+"use client";
 import { Import } from "lucide-react"; // Ensure to import this if you use it later
 import f from "../../../public/logo (1).svg"; // Ensure the path is correct
 import { Button } from "@/components/ui/button";
 import { NavbarResponsive } from "./NavbarResponsive";
-import {RsetLoggedIn,RsetSignedUp,selectLoggedIn,selectSignedUp} from "../../slices/NavabarSlice"
+import {
+  RsetLoggedIn,
+  RsetSignedUp,
+  selectLoggedIn,
+  selectSignedUp,
+} from "../../slices/NavbarSlices";
 import { useDispatch, useSelector } from "react-redux";
 import Link from "next/link";
+import DropdownMenu from "./DropDownNavbar";
+import { useState } from "react";
 
 const Navbar = () => {
-  const dispatch= useDispatch();
-  const loggedIn=useSelector(selectLoggedIn)
-  const signedUp=useSelector(selectSignedUp)
+  const dispatch = useDispatch();
+  const loggedIn = useSelector(selectLoggedIn);
+  const signedUp = useSelector(selectSignedUp);
+  
   const handleSignup = () => {
-    dispatch(RsetSignedUp(true))
-    dispatch(RsetLoggedIn(false))
-    window.location.href="/signUp"
-  }
+    // dispatch(RsetSignedUp(true));
+    // dispatch(RsetLoggedIn(false));
+    window.location.href = "/signUp";
+  };
   const handleLogin = () => {
-    dispatch(RsetLoggedIn(true))
-    dispatch(RsetSignedUp(false))
-    window.location.href="/login"
-  }
+    // dispatch(RsetLoggedIn(true));
+    // dispatch(RsetSignedUp(false));
+    window.location.href = "/login";
+  };
+  // const [islogged, setIslogged] = useState(false);
   return (
     <div
       id="container"
@@ -33,59 +41,61 @@ const Navbar = () => {
       }}
       //box-shadow: 0px 5px 42.6px 9px rgba(0, 0, 0, 0.36);
     >
-      <div id="left-section" className="flex-2 flex items-center lg:pl-12 md:pl-6">
-        
-        <Button
-          className="text-black  lg:h-[50px] lg:text-sm text-xs rounded-2xl absolute transition duration-200 hover:bg-[#a07f3b] bg-[#b09945] lg:w-[100px] w-[80px] ml-2"
-          onClick={handleSignup}
+      {!loggedIn &&
+        <div
+          id="left-section"
+          className="flex-2 flex items-center lg:pl-12 md:pl-6"
         >
-           
-          <img src="/person_add.svg" alt="" className="lg:"/> عضویت         
-          
-        </Button>
-        <Button
-          className="bg-white text-black lg:w-[190px] lg:h-[50px] rounded-2xl text-right justify-end hover:bg-gray-300 transition duration-200 lg:text-sm text-xs w-[150px] ml-2"
-          onClick={handleLogin}
-        >
-          
+          <Button
+            className="text-black  lg:h-[50px] lg:text-sm text-xs rounded-2xl absolute transition duration-200 hover:bg-[#a07f3b] bg-[#b09945] lg:w-[100px] w-[80px] ml-2"
+            onClick={handleSignup}
+          >
+            <img src="/person_add.svg" alt="" className="lg:" /> عضویت
+          </Button>
+          <Button
+            className="bg-white text-black lg:w-[190px] lg:h-[50px] rounded-2xl text-right justify-end hover:bg-gray-300 transition duration-200 lg:text-sm text-xs w-[150px] ml-2"
+            onClick={handleLogin}
+          >
             <img src="/login.svg" alt="" className="lg:mr-4 md:mr- mr-1 " />
-           ورود
-          
-  
-        </Button>
-      </div>
+            ورود
+          </Button>
+        </div>
+      
+      }
+      
+      {loggedIn && <DropdownMenu />}
+
       <div
         id="middle-section"
         className="flex-6 md:flex xl:text-xl md:text-lg sm:text-sm text-xs justify-center hidden items-center lg:space-x-5"
       >
-        <div className="flex-1 cursor-pointer  w-full text-center"><Link href="/contactus">
-            ارتباط با ما
-            </Link></div>
-        <div className="flex-1 cursor-pointer  text-center"><Link href="/#main-footer">
-            درباره ما
-            </Link></div>
-        <div className="flex-1 cursor-pointer  text-center"><Link href="/#customers-section">
-            مشتریان
-            </Link></div>
-        <div className="flex-1 cursor-pointer  text-center"><Link href="/#blog-container">
-            وبلاگ
-            </Link></div>
-        <div className="flex-1 cursor-pointer  text-center"><Link href="/#servicesCardsContainer">
-            طرح ها
-            </Link></div>
+        <div className="flex-1 cursor-pointer  w-full text-center">
+          <Link href="/contactus">ارتباط با ما</Link>
+        </div>
         <div className="flex-1 cursor-pointer  text-center">
-        <Link href="/">
-            خانه
-            </Link>
-          </div>
+          <Link href="/#main-footer">درباره ما</Link>
+        </div>
+        <div className="flex-1 cursor-pointer  text-center">
+          <Link href="/#customers-section">مشتریان</Link>
+        </div>
+        <div className="flex-1 cursor-pointer  text-center">
+          <Link href="/#blog-container">وبلاگ</Link>
+        </div>
+        <div className="flex-1 cursor-pointer  text-center">
+          <Link href="/#servicesCardsContainer">طرح ها</Link>
+        </div>
+        <div className="flex-1 cursor-pointer  text-center">
+          <Link href="/">خانه</Link>
+        </div>
       </div>
       <div id="right-section" className="lg:flex-2 md:flex-1 md:flex hidden">
-        <img src="logo (1).svg" alt="" className="lg:size-16 md:size-10 sm:size-8 size-6 flex-1 flex" />
+        <img
+          src="logo (1).svg"
+          alt=""
+          className="lg:size-16 md:size-10 sm:size-8 size-6 flex-1 flex"
+        />
       </div>
-      <div id="small-screen-right-section" className="md:hidden flex ">
-        <NavbarResponsive/>
-
-      </div>
+      <div id="small-screen-right-section" className="md:hidden flex "></div>
     </div>
   );
 };
