0e61be240e5a4e90783f86afa56a4fdf4881b008
Mohammad Zafari <my.mohammad.zafari@gmail.com>
2024-08-07 11:26:16 +0330

    changing navbar adding some more functionality and styles

--- src/components/Navbar/DropDownNavbar.tsx
+++ src/components/Navbar/DropDownNavbar.tsx
@@ -6,27 +6,37 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
+import { RsetLoggedIn } from "@/slices/NavbarSlices";
+import { useDispatch, UseDispatch } from "react-redux";
 
 
 const DropDownMenu = () => {
+  const dispatch= useDispatch()
+  const handleLoginChange =()=> {
+    {
+      dispatch(RsetLoggedIn(false)); // Make sure to use 'setLoggedIn' instead of 'RsetLoggedIn'
+    }
+  };
   return (
-<DropdownMenu >
-  <DropdownMenuTrigger  >
-    <div className="bg-white flex items-center rounded-3xl">
-    <img src="man-avatar.svg" alt="" className="size-12 w-fit bg-[#b09945] rounded-3xl m-1"/>
-    <p className="text-black m-1"> محمد ظفری</p>
+<div className="items-start">
+<DropdownMenu>
+  <DropdownMenuTrigger className="outline-none" >
+    <div className="flex items-center rounded-3xl border-white border-2 mt-1 outline-none" >
+    <img src="man-avatar.svg" alt="" className="size-10 w-fit bg-[#b09945] rounded-3xl m-1"/>
+    <p className="text-white m-1 px-2"> محمد ظفری</p>
+    
     </div>
   </DropdownMenuTrigger>
   <DropdownMenuContent className="bg-gray-400">
-    <DropdownMenuLabel>My Account</DropdownMenuLabel>
+    <DropdownMenuLabel>حساب من</DropdownMenuLabel>
     <DropdownMenuSeparator />
-    <DropdownMenuItem>Profile</DropdownMenuItem>
-    <DropdownMenuItem>Billing</DropdownMenuItem>
-    <DropdownMenuItem>Team</DropdownMenuItem>
-    <DropdownMenuItem>Subscription</DropdownMenuItem>
+    <DropdownMenuItem className="cursor-pointer">پروفایل
+    
+    </DropdownMenuItem>
+    <DropdownMenuItem className="cursor-pointer" onClick={handleLoginChange}>خروج از حساب</DropdownMenuItem>
   </DropdownMenuContent>
 </DropdownMenu>
-
+</div>
   );
 }
  
