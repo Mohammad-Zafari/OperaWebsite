6f32d770d91bcfa4d137acb25b882b180c131e4d
Mohammad Zafari <my.mohammad.zafari@gmail.com>
2024-08-11 11:16:05 +0330

    changing address map style

--- src/components/ContactUs/ContactUsPage.tsx
+++ src/components/ContactUs/ContactUsPage.tsx
@@ -26,10 +26,10 @@ const ContactUsPage = () => {
           </div>
           <div
             id="address"
-            className="flex-1 sm:flex-3 bg-[#333333] md:w-[70%] w-[70%] related flex flex-col items-end text-white font-light leading-relaxed rounded-b-2xl"
+            className="flex-1 sm:flex-3 bg-[#333333] bg-white md:w-[70%] w-[70%] related flex flex-col items-end text-gray-700 font-medium leading-relaxed rounded-b-2xl border-8 "
           >
             {/* when we use map rounded doesnt apply because of the map     */}
-            <div className="flex-2 flex items-center px-2 md:text-base text-sm rounded-b-full">
+            <div className="flex-2 flex items-center px-2 md:text-base text-sm rounded-b-full ">
               <p className="flex-12 text-right lg:pt-0 pt-4">
                 دفتر مرکزی: تهران، پاسداران، محله دروس، خیابان میربلوکی، پلاک 9،
                 واحد 101 دپارتمان فنی: تهران، دانشگاه علم و صنعت، ساختمان
@@ -37,17 +37,17 @@ const ContactUsPage = () => {
               </p>
               <img
                 src="location icon (1).svg"
-                className="flex-2 mx-2 lg:size-8 md:size-6 sm:size-5 size-4 sm:mb-5 md:mb-7 lg:mb-10"
+                className="flex-2 mx-2 lg:size-7 md:size-6 sm:size-5 size-4 sm:mb-5 md:mb-7 lg:mb-10"
                 alt=""
               />
             </div>
             <div className="flex-1 flex  px-2  md:text-base text-sm">
-              <p className="flex-12 text-right lg:pt-0 py-3 ">
+              <p className="flex-12 text-right lg:pt-0 py-3">
                 تلفن تماس: 02173227884 | 09224947358 | 09909928171
               </p>
               <img
                 src="phone icon.svg "
-                className="flex-2 lg:size-8 md:size-6 sm:size-5 size-4 mx-2 lg:mt-[-8px] sm:mt-2 mt-[11px]"
+                className="flex-2 lg:size-7 md:size-6 sm:size-5 size-4 mx-2 lg:mt-[-2px] sm:mt-2 mt-[11px]"
                 alt=""
               />
             </div>
