69a55bb7b1b42715f8d9307afd11f12766c8f9b5
AmirMahdi80 <m.ikani1380@gmail.com>
2024-08-12 12:04:23 +0330

    delete border

--- src/components/aboutUsVideo/AboutUsVideo.tsx
+++ src/components/aboutUsVideo/AboutUsVideo.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const AboutUsVideo = () => {
   return (
-    <div className="flex-col justify-center text-center py-16 md:py-28 lg:py-36 px-3 sm:px-8 md:px-40 lg:px-60 xl:px-80 border-2 border-gray-300 rounded-lg">
+    <div className="flex-col justify-center text-center py-16 md:py-28 lg:py-36 px-3 sm:px-8 md:px-40 lg:px-60 xl:px-80 rounded-lg">
       <div className="pb-16" dir="rtl">
         <h2 className="w-fit text-white text-xl sm:text-3xl border-b-2 pb-2">درباره ما</h2>
       </div>
