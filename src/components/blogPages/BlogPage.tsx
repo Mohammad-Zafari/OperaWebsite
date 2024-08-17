09b43f4a76e7eb5aea95140812cc9f54859ccb0d
parasa.gh <parsa.ghorbani11@gmail.com>
2024-08-17 12:11:42 +0330

    pictures changed in size

--- src/components/blogPages/BlogPage.tsx
+++ src/components/blogPages/BlogPage.tsx
@@ -26,7 +26,7 @@ const BlogPage: React.FC<info> = ({blogPost}) => {
     <img
       src={blogPost.imageSrc}
       alt=""
-      className="mx-auto w-full h-[500px] md:h-[700px]"
+      className="mx-auto w-full h-[500px] md:h-[600px]"
     />
     <div
       id="blogContainer"
@@ -34,22 +34,25 @@ const BlogPage: React.FC<info> = ({blogPost}) => {
       style={{ direction: "rtl" }}
     >
       <div id="title-container" className="justify-between items-center xl:px-16">
-        <div id="" className="text-center md:text-right py-8 md:pl-4">
-          <h1 className="text-4xl text-sky-400 mb-8">{blogPost.title}</h1>
-          <p className="text-purple-600 mb-8">
+        <div id="" className="text-center md:text-right py-4 md:pl-4">
+          <h1 className="text-4xl text-sky-400 mb-6">{blogPost.title}</h1>
+          <p className="text-purple-600">
             {blogPost.subtitle}
           </p>
-          <a href="/weblog" className="bg-sky-500 px-8 py-2 text-lg shadow-md shadow-black rounded-2xl hover:bg-sky-400 hover:shadow-none">
-            مشاهده سایر بلاگ‌ها
-          </a>
+
         </div>
         <div id="imageContainer" className="sm:px-16 md:px-0">
         </div>
       </div>
-      <div className="py-8 my-8 text-justify flex justify-center px-8 rounded-xl text-black">
+      <div className="py-8 text-justify flex justify-center px-8 rounded-xl text-purple">
         <p >
           {formatText(blogPost.description)}
         </p>
+      </div>
+      <div className="w-full text-center mb-8">
+        <a href="/weblog" className="bg-sky-500 px-40 py-2 text-lg shadow-md shadow-black rounded-2xl hover:bg-sky-400 hover:shadow-none">
+          مشاهده سایر بلاگ‌ها
+        </a>
       </div>
       <div className="flex justify-center w-full py-12 lg:px-32 2xl:px-52">
         <img className="rounded-lg" src="/hero-pattern.png" alt="video" />
